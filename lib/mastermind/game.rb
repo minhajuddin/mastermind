3b3b64f056f25c4e0d57b9c8a00b5bf0789bf7a4
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2010-06-10 15:10:51 -0700

    all specs and features passing

--- lib/mastermind/game.rb
+++ lib/mastermind/game.rb
@@ -5,12 +5,21 @@ module Mastermind
     end
 
     def start(code)
+      @code = code
       @messenger.puts "Welcome to Mastermind!"
       @messenger.puts "Enter guess:"
     end
 
     def guess(guess)
-      
+      result = Array.new
+      guess.each_with_index do |peg, index|
+        if(@code[index] == peg)
+          result << "b"
+        elsif(@code.include?(peg))
+          result << "w"
+        end
+      end
+      @messenger.puts result.sort.join
     end
   end
 end
--- spec/mastermind/game_spec.rb
+++ spec/mastermind/game_spec.rb
@@ -18,5 +18,39 @@ module Mastermind
         @game.start(%w[r y g c])
       end
     end
+
+    context "marking a guess" do
+
+      context "with all 4 colors correct in the correct places" do
+        it "should mark guesses with bbbb" do
+          messenger = mock("messenger").as_null_object
+          game = Mastermind::Game.new(messenger)
+          game.start(%w[r g y c])
+          messenger.should_receive(:puts).with("bbbb")
+          game.guess(%w[r g y c])
+        end
+      end
+
+      context "with all 4 colors correct and 2 in correct places" do
+        it "should mark guesses with bbww" do
+          messenger = mock("messenger").as_null_object
+          game = Mastermind::Game.new(messenger)
+          game.start(%w[r g y c])
+          messenger.should_receive(:puts).with("bbww")
+          game.guess(%w[r g c y])
+        end
+      end
+
+      context "with all 4 colors correct and 1 in correct place" do
+        it "should mark guesses with bwww" do
+          messenger = mock("messenger").as_null_object
+          game = Mastermind::Game.new(messenger)
+          game.start(%w[r g y c])
+          messenger.should_receive(:puts).with("bwww")
+          game.guess(%w[g c y r])
+        end
+      end
+
+    end
   end
 end
