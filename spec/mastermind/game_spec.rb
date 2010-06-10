3f10d044a54f2094f52654b85a855d7f05ce46f0
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2010-06-10 15:25:58 -0700

    refactored the code a bit

--- spec/mastermind/game_spec.rb
+++ spec/mastermind/game_spec.rb
@@ -2,11 +2,13 @@ require File.join(File.dirname(__FILE__), '/../spec_helper')
 
 module Mastermind
   describe Game do
+
+    before(:each) do
+      @messenger = mock("messenger").as_null_object
+      @game = Mastermind::Game.new(@messenger)
+    end
+
     context "starting up" do
-      before(:each) do
-        @messenger = mock("messenger").as_null_object
-        @game = Mastermind::Game.new(@messenger)
-      end
 
       it "should send a welcome message" do
         @messenger.should_receive(:puts).with("Welcome to Mastermind!")
@@ -23,31 +25,25 @@ module Mastermind
 
       context "with all 4 colors correct in the correct places" do
         it "should mark guesses with bbbb" do
-          messenger = mock("messenger").as_null_object
-          game = Mastermind::Game.new(messenger)
-          game.start(%w[r g y c])
-          messenger.should_receive(:puts).with("bbbb")
-          game.guess(%w[r g y c])
+          @game.start(%w[r g y c])
+          @messenger.should_receive(:puts).with("bbbb")
+          @game.guess(%w[r g y c])
         end
       end
 
       context "with all 4 colors correct and 2 in correct places" do
         it "should mark guesses with bbww" do
-          messenger = mock("messenger").as_null_object
-          game = Mastermind::Game.new(messenger)
-          game.start(%w[r g y c])
-          messenger.should_receive(:puts).with("bbww")
-          game.guess(%w[r g c y])
+          @game.start(%w[r g y c])
+          @messenger.should_receive(:puts).with("bbww")
+          @game.guess(%w[r g c y])
         end
       end
 
       context "with all 4 colors correct and 1 in correct place" do
         it "should mark guesses with bwww" do
-          messenger = mock("messenger").as_null_object
-          game = Mastermind::Game.new(messenger)
-          game.start(%w[r g y c])
-          messenger.should_receive(:puts).with("bwww")
-          game.guess(%w[g c y r])
+          @game.start(%w[r g y c])
+          @messenger.should_receive(:puts).with("bwww")
+          @game.guess(%w[g c y r])
         end
       end
 
